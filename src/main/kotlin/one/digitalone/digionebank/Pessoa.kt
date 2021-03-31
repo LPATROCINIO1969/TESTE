49f994f643b486b60464fc529cf9b886a45fc306
LUCIO PASSOS <lpatrocinio@terra.com.br>
2021-03-31 10:42:18 -0300

    update to apply the private modifier to set properties cpf

--- src/main/kotlin/one/digitalone/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalone/digionebank/Pessoa.kt
@@ -1,4 +1,21 @@
 package one.digitalone.digionebank
 
 class Pessoa {
+    var nome: String = "Lucio"
+    var cpf: String = "123.123.123-45"
+    private set
+
+    inner class Endereco {
+        var rua: String = "rua teste"
+    }
+
+
+}
+
+fun main() {
+    val Joao = Pessoa()
+//    Joao.cpf="343453"
+    println(Joao.nome)
+    println(Joao.cpf)
+    println(Joao.Endereco().rua)
 }
